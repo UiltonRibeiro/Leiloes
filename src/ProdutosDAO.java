b45a50c3ecec96645a07cabec9ff53b0f949021d
Uilton <uilton_3001@outlook.com>
2023-10-13 10:10:37 -0300

    Incrementando método de cadastrado no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,7 +12,10 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProdutosDAO {
@@ -23,9 +26,21 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        try {
+            String sql = "INSERT INTO produto(NOME,VALOR,STATUS) VALUES(?,?,?);";
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.execute();
+            prep.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Houve um erro ao cadastrar");
+            Logger.getLogger(ProdutosDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
-        //conn = new conectaDAO().connectDB();
         
         
     }
